f07308aba72917daee8a9115bdabd0a8f4407d30
Christiaan Bloemendaal <chrisbloemendaal@hotmail.com>
2015-11-15 16:39:04 +0100

    Added test scene + classes

---
+++ ComponentAttribute/Assets/Testing/DerivedTest.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DerivedTest : ExtendedBehaviour {
+
+    [Component]
+    public Transform Transform;
+    [Component]
+    public Light Light;
+    [Component]
+    public BoxCollider2D Collider;
+
+    [Component]
+    public Transform TransformP { get; set; }
+    [Component]
+    public Light LightP { get; set; }
+    [Component]
+    public BoxCollider2D ColliderP { get; set; }
+
+    private BoxCollider2D bcollider;
+    [Component]
+    protected BoxCollider2D BCollider { get { return bcollider; } }
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
---
+++ ComponentAttribute/Assets/Testing/ExtensionTest.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExtensionTest : MonoBehaviour {
+
+    [Component]
+    public Transform Transform;
+    [Component]
+    public Light Light;
+    [Component]
+    public BoxCollider2D Collider;
+
+    [Component]
+    public Transform TransformP { get; set; }
+    [Component]
+    public Light LightP { get; set; }
+    [Component]
+    public BoxCollider2D ColliderP { get; set; }
+
+    private BoxCollider2D bcollider;
+    [Component]
+    protected BoxCollider2D BCollider { get { return bcollider; } }
+
+    void Start () {
+        // You have to call LoadComponents with this. for it to work
+        this.LoadComponents();
+	}
+	
+	void Update () {
+	
+	}
+}
